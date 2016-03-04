b6f9d2cc8c0c1c7774c1d95979b07554499e1a2b
Alex Wilson <alex.wilson@joyent.com>
2016-03-04 12:20:20 -0800

    Add a test, fix up some things

---
+++ test/google.test.js
@@ -0,0 +1,41 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2016, Joyent, Inc.
+ */
+
+const mod_tape = require('tape');
+const mod_nsc = require('../lib/index');
+const mod_net = require('net');
+
+mod_tape.test('look up google.com with 8.8.8.8', function (t) {
+	var req = new mod_nsc.DnsMessage();
+	req.addQuestion('google.com', 'A');
+	t.ok(req.validate());
+
+	req.on('error', function (err) {
+		t.ifError(err);
+	});
+	req.on('reply', function (msg, done) {
+		t.ok(!msg.isError());
+		var ans = msg.getAnswers();
+		t.ok(Array.isArray(ans));
+		t.ok(ans.length > 0);
+		t.strictEqual(ans[0].name, 'google.com');
+		t.strictEqual(ans[0].type, 'A');
+		t.ok(mod_net.isIPv4(ans[0].target));
+		done();
+		t.end();
+	});
+
+	var sock = new mod_nsc.DnsUdpSocket({ family: 'udp4' });
+	sock.on('ready', function () {
+		sock.send(req, { address: '8.8.8.8', port: 53 });;
+		sock.end();
+	});
+	sock.on('error', function (err) {
+		t.ifError(err);
+	});
+});
